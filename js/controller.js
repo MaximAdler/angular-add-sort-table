330e9640312fc263dff8375211dfcbc552fc898a
MaximAdler <orelmax@i.ua>
2017-02-02 18:57:19 +0200

    Fix geolocation(distance between current position and person position)

--- js/controller.js
+++ js/controller.js
@@ -9,74 +9,11 @@ test.controller('mainController', function($scope, $http, $log, dataService, $ro
 
     dataService.getData().then(function(data) {
       $scope.people = data;
+      $log.info('Data is uploaded')
     });
 
     $scope.$watch('people', function(newValue, oldValue) {
         if (newValue != oldValue) dataService.setData(newValue)
+        // $log.info($scope.people)
     })
-
-
-    // $scope.addPerson = function(name, username, email, street, suite, city, zipcode, lat, lng, phone, website, coName, catchPhrase, bs) {
-    //     if (this.people.name) {
-    //         $scope.people.push({
-    //             id: $scope.people.length + 1,
-    //             name: $scope.people.name,
-    //             username: $scope.people.username,
-    //             email: $scope.people.email,
-    //             street: $scope.people.address.street,
-    //             suite: $scope.people.address.suite,
-    //             city: $scope.people.address.city,
-    //             zipcode: $scope.people.address.zipcode,
-    //             lat: $scope.people.address.geo.lat,
-    //             lng: $scope.people.address.geo.lng,
-    //             phone: $scope.people.phone,
-    //             website: $scope.people.website,
-    //             coName: $scope.people.company.name,
-    //             catchPhrase: $scope.people.company.catchPhrase,
-    //             bs: $scope.people.company.bs
-    //         })
-    //         $scope.people.name = '';
-    //         $scope.people.username = '';
-    //         $scope.people.email = '';
-    //         $scope.people.address.street = '';
-    //         $scope.people.address.suite = '';
-    //         $scope.people.address.city = '';
-    //         $scope.people.address.zipcode = '';
-    //         $scope.people.address.geo.lat = '';
-    //         $scope.people.address.geo.lng = '';
-    //         $scope.people.phone = '';
-    //         $scope.people.website = '';
-    //         $scope.people.company.name = '';
-    //         $scope.people.company.catchPhrase = '';
-    //         $scope.people.company.bs = '';
-    //     }
-    // }
-
-    //position...
-    // http://stackoverflow.com/questions/13840516/how-to-find-my-distance-to-a-known-location-in-javascript
-
-    //fix
-
-    $scope.getDistance = function(lat1, lng1, lat, lng) {
-        lat1 = $scope.people.address.geo.lat;
-        lng1 = $scope.people.address.geo.lng;
-        window.navigator.geolocation.getCurrentPosition(function(pos) {
-            console.log(pos);
-            lat = pos.coords.latitude;
-            lng = pos.coords.longitude;
-        })
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat1 - lat); // deg2rad below
-        var dLng = deg2rad(lng1 - lng);
-        var a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(deg2rad(lat)) * Math.cos(deg2rad(lat1)) *
-            Math.sin(dLng / 2) * Math.sin(dLng / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c; // Distance in km
-        console.log(d)
-        return d;
-    }
-
-
 })
--- js/modalWinController.js
+++ js/modalWinController.js
@@ -1,48 +1,41 @@
 var test = angular.module('test');
 
-test.controller('ModalDemoCtrl', ['$http','$scope','$uibModal', '$log','$document', function ($http, $scope, $uibModal, $log, $document, Data) {
+test.controller('ModalDemoCtrl', function ($scope, $uibModal, $log, $document, dataService) {
   var $ctrl = this;
 
 
-  $scope.addPerson = function(name, username, email, street, suite, city, zipcode, lat, lng, phone, website, coName, catchPhrase, bs) {
-      if (this.people.name) {
-          $scope.people.push({
-              id: $scope.people.length + 1,
-              name: $scope.people.name,
-              username: $scope.people.username,
-              email: $scope.people.email,
-              street: $scope.people.address.street,
-              suite: $scope.people.address.suite,
-              city: $scope.people.address.city,
-              zipcode: $scope.people.address.zipcode,
-              lat: $scope.people.address.geo.lat,
-              lng: $scope.people.address.geo.lng,
-              phone: $scope.people.phone,
-              website: $scope.people.website,
-              coName: $scope.people.company.name,
-              catchPhrase: $scope.people.company.catchPhrase,
-              bs: $scope.people.company.bs
-          })
-          $scope.people.name = '';
-          $scope.people.username = '';
-          $scope.people.email = '';
-          $scope.people.address.street = '';
-          $scope.people.address.suite = '';
-          $scope.people.address.city = '';
-          $scope.people.address.zipcode = '';
-          $scope.people.address.geo.lat = '';
-          $scope.people.address.geo.lng = '';
-          $scope.people.phone = '';
-          $scope.people.website = '';
-          $scope.people.company.name = '';
-          $scope.people.company.catchPhrase = '';
-          $scope.people.company.bs = '';
-      }
-  }
 
-  //
-  $ctrl.items = [];
-  //
+  dataService.getData().then(function(data) {
+    $ctrl.people = data;
+  });
+
+  $scope.$watch('people', function(newValue, oldValue) {
+      if (newValue != oldValue) dataService.setData(newValue)
+  })
+
+
+  $ctrl.addPerson = function(name, username, email, street, suite, city, zipcode, lat, lng, phone, website, coName, catchPhrase, bs) {
+      if (this.people.name) {
+          $ctrl.people.push({
+              id: $ctrl.people.length + 1,
+              name: $ctrl.people.name,
+              username: $ctrl.people.username,
+              email: $ctrl.people.email,
+              street: $ctrl.people.address.street,
+              suite: $ctrl.people.address.suite,
+              city: $ctrl.people.address.city,
+              zipcode: $ctrl.people.address.zipcode,
+              lat: $ctrl.people.address.geo.lat,
+              lng: $ctrl.people.address.geo.lng,
+              phone: $ctrl.people.phone,
+              website: $ctrl.people.website,
+              coName: $ctrl.people.company.name,
+              catchPhrase: $ctrl.people.company.catchPhrase,
+              bs: $ctrl.people.company.bs
+          })
+        }
+      }
+  // $ctrl.newPerson ={}
 
   $ctrl.open = function (size, parentSelector) {
     var parentElem = parentSelector ? angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
@@ -50,18 +43,22 @@ test.controller('ModalDemoCtrl', ['$http','$scope','$uibModal', '$log','$documen
       animation: true,
       ariaLabelledBy: 'modal-title',
       ariaDescribedBy: 'modal-body',
-      templateUrl: './template/myModalContent.html',
+      templateUrl: './template/ModalContent.html',
       controller: 'ModalInstanceCtrl',
       controllerAs: '$ctrl',
       size: size,
       appendTo: parentElem,
       resolve: {
-        items: function () {
-          return $ctrl.items;
+        people: function () {
+          return $ctrl.people;
         }
       }
     });
-
+    modalInstance.result.then(function () {
+      $ctrl.addPerson();
+    }, function () {
+      $log.info('Modal don\'t want to add new Person');
+    });
   };
 
   $ctrl.openComponentModal = function () {
@@ -69,34 +66,121 @@ test.controller('ModalDemoCtrl', ['$http','$scope','$uibModal', '$log','$documen
       animation: true,
       component: 'modalComponent',
       resolve: {
-        items: function () {
-          return $ctrl.items;
+        people: function () {
+          return $ctrl.people;
         }
       }
     });
   };
-}]);
 
-test.controller('ModalInstanceCtrl',['$uibModalInstance','items','$scope', '$rootScope', function ($uibModalInstance, items, $scope, $rootScope) {
+  // $scope.$watch('people', function(newValue, oldValue) {
+  //     if (newValue != oldValue) dataService.setData(newValue)
+  // })
+  // dataService.setData();
+
+
+});
+
+test.controller('ModalInstanceCtrl',function (dataService, $uibModalInstance, people, $scope, $rootScope) {
   var $ctrl = this;
-  $ctrl.items = items;
+  $ctrl.people = people;
+  // $ctrl.newPerson = newPerson;
+
+//what if another function
+  // $ctrl.addPerson = function(name, username, email, street, suite, city, zipcode, lat, lng, phone, website, coName, catchPhrase, bs) {
+  //     if (this.people.name) {
+  //         $ctrl.people.push({
+  //             id: $ctrl.people.length + 1,
+  //             name: $ctrl.people.name,
+  //             username: $ctrl.people.username,
+  //             email: $ctrl.people.email,
+  //             street: $ctrl.people.address.street,
+  //             suite: $ctrl.people.address.suite,
+  //             city: $ctrl.people.address.city,
+  //             zipcode: $ctrl.people.address.zipcode,
+  //             lat: $ctrl.people.address.geo.lat,
+  //             lng: $ctrl.people.address.geo.lng,
+  //             phone: $ctrl.people.phone,
+  //             website: $ctrl.people.website,
+  //             coName: $ctrl.people.company.name,
+  //             catchPhrase: $ctrl.people.company.catchPhrase,
+  //             bs: $ctrl.people.company.bs
+  //         })
+  //         $ctrl.people.name = '';
+  //         $ctrl.people.username = '';
+  //         $ctrl.people.email = '';
+  //         $ctrl.people.address.street = '';
+  //         $ctrl.people.address.suite = '';
+  //         $ctrl.people.address.city = '';
+  //         $ctrl.people.address.zipcode = '';
+  //         $ctrl.people.address.geo.lat = '';
+  //         $ctrl.people.address.geo.lng = '';
+  //         $ctrl.people.phone = '';
+  //         $ctrl.people.website = '';
+  //         $ctrl.people.company.name = '';
+  //         $ctrl.people.company.catchPhrase = '';
+  //         $ctrl.people.company.bs = '';
+  //     }
+  // }
+
 
 //submit must be there
-  $ctrl.ok = function () {
+  $ctrl.ok = function (name, username, email, street, suite, city, zipcode, lat, lng, phone, website, coName, catchPhrase, bs) {
     //fix
-    $scope.$emit('addPerson',{})
-    $uibModalInstance.close('closed');
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..
+if (this.people.name) {
+    $ctrl.people.push({
+        id: $ctrl.people.length + 1,
+        name: $ctrl.people.name,
+        username: $ctrl.people.username,
+        email: $ctrl.people.email,
+        street: $ctrl.people.address.street,
+        suite: $ctrl.people.address.suite,
+        city: $ctrl.people.address.city,
+        zipcode: $ctrl.people.address.zipcode,
+        lat: $ctrl.people.address.geo.lat,
+        lng: $ctrl.people.address.geo.lng,
+        phone: $ctrl.people.phone,
+        website: $ctrl.people.website,
+        coName: $ctrl.people.company.name,
+        catchPhrase: $ctrl.people.company.catchPhrase,
+        bs: $ctrl.people.company.bs
+    })
+  }
+    // $ctrl.people.name = '';
+    // $ctrl.people.username = '';
+    // $ctrl.people.email = '';
+    // $ctrl.people.address.street = '';
+    // $ctrl.people.address.suite = '';
+    // $ctrl.people.address.city = '';
+    // $ctrl.people.address.zipcode = '';
+    // $ctrl.people.address.geo.lat = '';
+    // $ctrl.people.address.geo.lng = '';
+    // $ctrl.people.phone = '';
+    // $ctrl.people.website = '';
+    // $ctrl.people.company.name = '';
+    // $ctrl.people.company.catchPhrase = '';
+    // $ctrl.people.company.bs = '';
+// }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..
+// $scope.$watch('people', function(newValue, oldValue) {
+//     if (newValue != oldValue) dataService.setData(newValue)
+// })
+console.log($ctrl.people)
+
+    $uibModalInstance.close($ctrl.people);
     // $rootScope.$emit('addPersonRoot',{});
   };
 
   $ctrl.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
-}]);
+});
 
 
 test.component('modalComponent', {
-  templateUrl: './template/myModalContent.html',
+  templateUrl: './template/ModalContent.html',
   bindings: {
     resolve: '<',
     close: '&',
@@ -105,9 +189,14 @@ test.component('modalComponent', {
   controller: function () {
     var $ctrl = this;
 
+    $ctrl.$onInit = function () {
+      $ctrl.people = $ctrl.resolve.people;
+    };
+
     $ctrl.ok = function () {
       //main mb
-      $ctrl.close({$value: 1});
+      //mb addPerson()
+      $ctrl.close({$value: $ctrl.people});
           // $rootScope.$emit('addPersonRoot',{});
     };
 
